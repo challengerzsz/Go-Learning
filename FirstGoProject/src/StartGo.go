ae01fc82a7fd0d7b675519182779a9e046106bec
challengerZeng <664903471@qq.com>
2018-05-31 10:37:49 +0800

    go语言第一次web实战..

--- FirstGoProject/src/StartGo.go
+++ FirstGoProject/src/StartGo.go
@@ -1,8 +1,39 @@
-package src
+package main
 
-import "fmt"
+import (
+	"net/http"
+	"fmt"
+	"strings"
+	"log"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	//解析参数
+	r.ParseForm()
+	//输出到服务端的打印信息
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+
+	for k, v := range r.Form {
+		fmt.Println("key", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+
+	//写入响应
+	fmt.Fprint(w, "Hello Go")
+
+}
+
 
 func main()  {
-	//第一个简单Go程序
-	fmt.Print("Hello Go")
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		 log.Fatal("ListenAndServe", err)
+	}
 }
+
+
+

dc8c3023fca9ff25be5c2bc65fb5f535679f1872
challengerZeng <664903471@qq.com>
2019-04-28 20:38:56 +0800

    go project test push

--- ManageProject/src/calc/calc.go
+++ ManageProject/src/calc/calc.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"simplemath"
 )
 
 var Usage = func() {
@@ -30,7 +31,7 @@ func main() {
 			fmt.Println("USAGE: calc sqrt <integer1><integer2>")
 			return
 		}
-		ret := Add(v1, v2)
+		ret := simplemath.Add(v1, v2)
 		fmt.Println("Result: ", ret)
 
 	case "sqrt":
@@ -42,6 +43,11 @@ func main() {
 		v, err := strconv.Atoi(args[1])
 		if err != nil {
 			fmt.Println("USAGE: cllc sqrt <integer>")
+			return
 		}
+		ret := simplemath.Sqrt(v)
+		fmt.Println("Result: ", ret)
+	default:
+		Usage()
 	}
 }
--- ManageProject/src/simplemath/add.go
+++ ManageProject/src/simplemath/add.go
@@ -1,7 +1,5 @@
-package main
+package simplemath
 
 func Add(a int, b int) int {
 	return a + b
 }
-
-

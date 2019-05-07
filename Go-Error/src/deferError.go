4900e923590a3d737ceeb3b2738ca132b402b7e3
zengshuaizhi <challengerzsz@126.com>
2019-05-07 18:26:29 +0800

    defer & recover()

--- Go-Error/src/deferError.go
+++ Go-Error/src/deferError.go
@@ -2,6 +2,7 @@ package src
 
 import (
 	"io"
+	"log"
 	"os"
 )
 
@@ -20,6 +21,17 @@ func CopyFile(dst, src string) (w int64, err error) {
 	}
 
 	defer dstFile.Close()
+
+	// defer声明一个func 可以写很多的出现异常时关闭资源等等的动作 无论是否发生异常这个匿名函数都会被执行
+	defer func() {
+
+		//srcFile.Close()
+		//dstFile.Close()
+
+		if r := recover(); r != nil {
+			log.Printf("Runtime error caught: %v", r)
+		}
+	}()
 
 	return io.Copy(dstFile, srcFile)
 }

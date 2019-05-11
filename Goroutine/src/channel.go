e862df9168882d1c3306e8edb7abd1a60f96e640
zengshuaizhi <challengerzsz@126.com>
2019-05-11 21:22:19 +0800

    go routine & channel

---
+++ Goroutine/src/channel.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func Count1(ch chan int) {
+	fmt.Println("Counting")
+	// 在这个channel被读取之前这个写入操作是阻塞的
+	ch <- 1
+}
+
+func main() {
+	chs := make([]chan int, 10)
+	for i := 0; i < 10; i++ {
+		chs[i] = make(chan int)
+		go Count1(chs[i])
+	}
+
+	for _, ch := range chs {
+		// 在这个channel写入数据之前读取操作也是阻塞的
+		<-ch
+	}
+}

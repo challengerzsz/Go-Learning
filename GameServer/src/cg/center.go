6187877a8aa3ab26fae961e42e52a9726c02178f
zengshuaizhi <challengerzsz@126.com>
2019-05-20 22:16:47 +0800

    center_client push

--- GameServer/src/cg/center.go
+++ GameServer/src/cg/center.go
@@ -2,6 +2,7 @@ package cg
 
 import (
 	"encoding/json"
+	"errors"
 	"ipc"
 	"sync"
 )
@@ -52,7 +53,88 @@ func (server *CenterServer) removePlayer(params string) error {
 		if v.Name == params {
 			if len(server.players) == 1 {
 				server.players = make([]*Player, 0)
+			} else if i == len(server.players)-1 {
+				server.players = server.players[:i]
+			} else if i == 0 {
+				server.players = server.players[1:]
+			} else {
+				server.players = append(server.players[:i-1], server.players[:i+1]...)
 			}
 		}
+
+		return nil
 	}
+
+	return errors.New("Player is not found.")
+}
+
+func (server *CenterServer) listPlayer(param string) (players string, err error) {
+	server.mutex.Lock()
+	defer server.mutex.Unlock()
+
+	if len(server.players) > 0 {
+		b, _ := json.Marshal(server.players)
+		players = string(b)
+	} else {
+		err = errors.New("No player online")
+	}
+
+	return
+}
+
+func (server *CenterServer) broadcast(params string) error {
+	var message Message
+	err := json.Unmarshal([]byte(params), &message)
+	if err != nil {
+		return err
+	}
+
+	server.mutex.Lock()
+	defer server.mutex.Unlock()
+
+	if len(server.players) > 0 {
+		for _, player := range server.players {
+			player.mq <- &message
+		}
+	} else {
+		err = errors.New("No player online")
+	}
+
+	return err
+}
+
+func (server *CenterServer) Handle(method, params string) *ipc.Response {
+
+	switch method {
+	case "addPlayer":
+		err := server.addPlayer(params)
+		if err != nil {
+			return &ipc.Response{Code: err.Error()}
+		}
+	case "removePlayer":
+		err := server.removePlayer(params)
+		if err != nil {
+			return &ipc.Response{Code: err.Error()}
+		}
+	case "listPlayer":
+		players, err := server.listPlayer(params)
+		if err != nil {
+			return &ipc.Response{Code: err.Error()}
+		}
+		return &ipc.Response{"200", players}
+	case "broadcast":
+		err := server.broadcast(params)
+		if err != nil {
+			return &ipc.Response{Code: err.Error()}
+		}
+		return &ipc.Response{Code: "200"}
+	default:
+		return &ipc.Response{Code: "404", Body: method + " : " + params}
+	}
+
+	return &ipc.Response{Code: "200"}
+}
+
+func (server *CenterServer) Name() string {
+	return "CenterServer"
 }
